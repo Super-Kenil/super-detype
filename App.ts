fd5b64bce9a58e5cf41046372b79c2d49754be2a
Super-Kenil <kenilsudani.blog@gmail.com>
2024-03-06 18:46:31 +0530

    FEAT: Successfully Compiling TS code to JS

--- App.ts
+++ App.ts
@@ -2,6 +2,7 @@ import fs from 'fs-extra'
 import { program } from 'commander'
 import { execSync } from 'child_process'
 import { removeTypes } from 'remove-types'
+import { transformSync } from '@babel/core'
 import * as path from 'path'
 
 const directoryPath = './dist/JS'
@@ -16,11 +17,16 @@ async function processFiles (directory: string) {
       if (fs.statSync(filePath).isDirectory()) {
         await processFiles(filePath)
       } else {
-        if (file.endsWith('.js') || file.endsWith('.jsx')) {
+        if (file.endsWith('.ts') || file.endsWith('.tsx')) {
           const fileContent = await fs.readFile(filePath, { encoding: 'utf-8' })
-          const typesRemovedContent = await removeTypes(fileContent, false)
+          const typesRemovedContent = await transformSync(fileContent, {
+            compact: false,
+            presets: ['@babel/preset-typescript'],
+            filename: filePath
+          })
+          // const typesRemovedContent = await removeTypes(fileContent, false)
           console.log('filepath', filePath)
-          await fs.writeFile(filePath, typesRemovedContent, { encoding: 'utf-8', })
+          await fs.writeFile(filePath, typesRemovedContent?.code ?? 'ERROR: CONVERTING FILE KENIL', { encoding: 'utf-8', })
           // console.log(`Content of ${file}:`)
           // console.log(fileContent)
         }
@@ -38,10 +44,11 @@ async function processFiles (directory: string) {
 
   try {
     fs.copySync('TS', 'dist/JS',)
-    execSync('find ./dist/JS -type f -name "*.tsx" -exec sh -c \'mv "$1" "${1%.tsx}.jsx"\' _ {} \\;')
-    execSync('find ./dist/JS -type f -name "*.ts" -exec sh -c \'mv "$1" "${1%.ts}.js"\' _ {} \\;')
 
     await processFiles(directoryPath)
+
+    execSync('find ./dist/JS -type f -name "*.tsx" -exec sh -c \'mv "$1" "${1%.tsx}.jsx"\' _ {} \\;')
+    execSync('find ./dist/JS -type f -name "*.ts" -exec sh -c \'mv "$1" "${1%.ts}.js"\' _ {} \\;')
 
   } catch (error) {
     if (error instanceof Error) {
