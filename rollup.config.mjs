bc4bca6b4bb013acb505423e14e37fe0097790af
Super-Kenil <kenilsudani.blog@gmail.com>
2024-04-26 19:11:25 +0530

    FIX: removed unwanted packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
 
 /** @type {import('rollup').RollupOptions} */
 export default {
@@ -21,7 +20,6 @@ export default {
       presets: ['@babel/preset-typescript'],
     }),
     commonjs(),
-    json(),
     nodeResolve(),
   ],
 }
--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig([
-	{
-		entry: ["src/app.ts"],
-		format: ["cjs"],
-		target: "node18",
-	},
-]);
